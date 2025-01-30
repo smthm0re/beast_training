997a9d3e11bef6202657c511550d4609f15a2a73
smthm0re <ivansergeevichalexeev@gmail.com>
2025-01-30 09:49:06 +0300

    Работает. Нужно поправить дизайн.

---
+++ lib/models/training.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'training.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TrainingAdapter extends TypeAdapter<Training> {
+  @override
+  final int typeId = 1;
+
+  @override
+  Training read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Training(
+      id: fields[0] as int,
+      date: fields[1] as DateTime,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Training obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.date);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TrainingAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
