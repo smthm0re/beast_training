ab1394c9bad7121e7e6d59abb2d9bfed72aa38a2
smthm0re <ivansergeevichalexeev@gmail.com>
2025-02-12 23:44:18 +0300

    Удалил firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:beast_training/beast_training_app.dart';
-import 'package:beast_training/firebase_options.dart';
 import 'package:beast_training/models/exercise.dart';
 import 'package:beast_training/models/training.dart';
 import 'package:beast_training/ui-kit/notification_service/notification_service.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -15,10 +13,6 @@ void main() async {
   Hive.registerAdapter(ExerciseAdapter());
   await Hive.openBox("defaultBox");
   await Hive.openBox("exerciseBox");
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 
   await notificationService.init();
 
